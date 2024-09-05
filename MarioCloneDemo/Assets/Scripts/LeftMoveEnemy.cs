179b9a9672a92d5e576dca2d3d0b430067479ea4
B-Sheley <112135501+B-Sheley@users.noreply.github.com>
2024-09-05 17:58:59 -0400

    commit

--- MarioCloneDemo/Assets/Scripts/LeftMoveEnemy.cs
+++ MarioCloneDemo/Assets/Scripts/LeftMoveEnemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LeftMoveEnemy : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    private int movePoint = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +16,22 @@ public class LeftMoveEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-1 * Vector2.right * moveSpeed * Time.deltaTime);
+        transform.Translate(movePoint * Vector2.right * moveSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+        if (collision.gameObject.tag == "Player")
+        {
+            //Destroy(collision.gameObject);
+            Debug.Log("You been hit");
+        }
+        if (collision.gameObject.tag == "wall")
+        {
+            Debug.Log("change");
+            movePoint *= -1;
+        }
     }
+
+
 }
--- MarioCloneDemo/Assets/Scripts/Platform.cs
+++ MarioCloneDemo/Assets/Scripts/Platform.cs
@@ -2,21 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Platform : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    [SerializeField] float speed;
+    private float startLocation;
+    private float endLocation;
+    [SerializeField] float distance;
+    private Vector3 direction = Vector3.up;
+    private bool playerLanded = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        startLocation = transform.position.y;
+        endLocation = startLocation + distance;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerLanded == true) {
+            transform.Translate(direction * speed * Time.deltaTime);
+
+            if (transform.position.y >= endLocation) { direction = Vector3.down; }
+            if (transform.position.y <= startLocation) { direction = Vector3.up; }
+        }
         
-        transform.Translate(-1 * Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerLanded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerLanded = false;
+        }
     }
 }
